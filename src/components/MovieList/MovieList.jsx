f2388544a0783c0a5dfdc0959e2f65f54acb0a82
deadkor121 <143342877+deadkor121@users.noreply.github.com>
2024-04-08 23:33:11 +0300

    Update MovieList.jsx
    Fix location

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,20 +1,25 @@
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import clsx from "clsx";
-
 import style from "./MovieList.module.css";
 
 const MovieList = ({ moviesList }) => {
+  const location = useLocation();
+
   return (
     <ul className={clsx(style.list)}>
       {Array.isArray(moviesList) &&
         moviesList.map((item) => {
           return (
             <li className={clsx(style.item)} key={item.id}>
-              <NavLink to={`/movies/${item.id}`}>{item.title}</NavLink>
+              <NavLink to={`/movies/${item.id}`} state={{ location }}>
+                {item.title}
+              </NavLink>
             </li>
           );
         })}
     </ul>
   );
 };
+
 export default MovieList;
