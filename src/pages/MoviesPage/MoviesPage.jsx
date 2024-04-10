00da8afab6fa2995654f53ad0bb789f6a926459d
deadkor121 <143342877+deadkor121@users.noreply.github.com>
2024-04-10 15:35:23 +0300

    Update MoviesPage.jsx
    .

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,99 +1,104 @@
-import toast, { Toaster } from "react-hot-toast";
-import { useState, useEffect, lazy } from "react";
-import clsx from "clsx";
-import { fetchSearchMovies } from '../../filmsApi';
-const ErrorMessagе = lazy(() => import("../../components/ErrorMessage/ErrorMessage"));
-const MovieList = lazy(() => import("../../components/MovieList/MovieList"));
-import Loader from "../../components/Loader/Loader";
-import style from "./MoviesPage.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+ import { Field, Form, Formik } from 'formik';
+ import { fetchSearchMovies } from '../../filmsApi';
+ import Loader from '../../components/Loader/Loader';
+ import MovieList from '../../components/MovieList/MovieList';
+ import { useSearchParams } from 'react-router-dom';
 
-const MoviesPage = () => {
-  const [arrMovies, setArrMovies] = useState([]);
-  const [showLoader, setShowLoader] = useState(false);
-  const [showError, setShowError] = useState(false);
-  
+ const MoviesPage = () => {
+  const [searchQuery, setSearchQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const searchParams = useSearchParams();
-  
-  const fetchData = async (query) => {
-    if (query !== '') {
-      try {
-        setIsLoading(true);
+   const [error, setError] = useState(false);
+   const [params, setParams] = useSearchParams();
+
+   const filmQuery = params.get('query') ?? '';
+
+   useEffect(() => {
+     if (searchQuery === '') {
+       return;
+     }
+
+     async function getMoviesbySearchQuery() {
+       try {
+         setIsLoading(true);
         setError(false);
-        const dataResp = await fetchSearchMovies(query);
-        if (dataResp.length === 0) {
+         const dataResp = await fetchSearchMovies(searchQuery);
+         if (dataResp.length === 0) {
           setError('No movies found per your request');
-        }
-        setMovies(dataResp);
+         }
+         setMovies(dataResp);
       } catch (error) {
-        setError('Oops, http issue!');
-      } finally {
+         setError('Oops, http issue!');
+       } finally {
         setIsLoading(false);
+       }
+     }
+
+     getMoviesbySearchQuery();
+   }, [searchQuery]);
+
+   useEffect(() => {
+     async function getMovies() {
+       if (filmQuery !== '') {
+         try {
+           setIsLoading(true);
+           setError(false);
+           const dataResp = await fetchSearchMovies(filmQuery);
+           setMovies(dataResp);
+         } catch (error) {
+           setError('Oops, http issue!');
+        } finally {
+           setIsLoading(false);
+        }
       }
     }
-  }
 
-  useEffect(() => {
-    const searchQuery = searchParams.get('query') || '';
-    fetchData(searchQuery);
-  }, [searchParams]);
+     getMovies();
+   }, [filmQuery]);
 
-  const handleSearch = async (newQuery) => {
-    if (newQuery.trim() === '') {
-      setError('The query is empty, please input a search request');
-      return;
-    }
+   const handleSearch = newQuery => {
+     if (newQuery.trim() === '') {
+       setError('The query is empty, please input search request');
+       return;
+     }
+     setMovies([]);
+     setSearchQuery(newQuery);
+     setParams(params => ({
+       ...params,
+        we use the name to tell Formik which key of `values` to update
+       query: newQuery,
+    }));
+     setSearchQuery('');
+   };
 
-    setMovies([]); // Clear previous search results
-    setSearchParams({ query: newQuery });
-  }
+   return (
+     <div>
+       <Formik
+         initialValues={{ query: searchQuery }}
+         onSubmit={(values, actions) => {
+           handleSearch(values.query);
+           actions.resetForm();
+        }}
+        
+        <Form>
+           <Field name="query" placeholder="Search movies" />
+           <button type="submit">Search</button>
+         </Form>
+       </Formik>
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const { search } = form.elements;
-    if (search.value.length === 0) {
-      toast.error("The input field is empty! Please write a word to search.", { icon: "😰" });
-    } else {
-      handleSearch(search.value);
-    }
-  };
-
-  return (
-    <>
-      <div className={style.searchBox}>
-        <form className={style.moviesForm} onSubmit={handleSubmit}>
-          <input
-            className={style.moviesInput}
-            type="text"
-            name="search"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search films and movies"
-          />
-          <button className={style.formButton} type="submit">
-            Search
-          </button>
-        </form>
-        <Toaster position="top-right" reverseOrder={false} />
-      </div>
-      {showLoader && <Loader />}
-      {showError ? (
-        <ErrorMessagе />
-      ) : (
-        <div className={style.searchContainer}>
-          <MovieList moviesList={arrMovies} />
-        </div>
+       {error ? (
+         <div>{error}</div>
+       ) : (
+         <>
+           {!isLoading && !error && movies.length > 0 && (
+            <MovieList movies={movies} />
+           )}
+           {isLoading && <Loader />}
+         </>
       )}
-      <>
-        {!isLoading && movies.length > 0 && <MovieList movies={movies} />}
-        {isLoading && <Loader />}
-      </>
-    </>
-  );
-};
+     </div>
+   );
+ };
 
-export default MoviesPage; 
+ export default MoviesPage;
